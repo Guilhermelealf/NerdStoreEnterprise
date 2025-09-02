6f81fc8592a39eb2ae24cdb08ae47b2c88749de5
Guilherme Ferreira Leal <guilherme.fleal2003@gmail.com>
2025-09-02 12:46:06 -0300

    Adicionar arquivos de projeto.

---
+++ src/services/NSE.Identidade.API/Data/AplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Identidade.API.Data
+{
+    public class AplicationDbContext : IdentityDbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
+    }
+}
---
+++ src/services/NSE.Identidade.API/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NSE.Identidade.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
