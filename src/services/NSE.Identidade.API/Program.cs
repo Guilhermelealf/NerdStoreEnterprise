bc0e07abfdce4e30ef8f1bc954374b2abe170051
Guilherme Ferreira Leal <guilherme.fleal2003@gmail.com>
2025-09-04 09:43:04 -0300

    Criação das classes IdentityConfig e DbConfig Configurado Jwt

--- src/services/NSE.Identidade.API/Program.cs
+++ src/services/NSE.Identidade.API/Program.cs
@@ -2,17 +2,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Configuration;
-using NSE.Identidade.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<AplicationDbContext>()
-    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 
@@ -26,11 +20,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c=>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    });
 }
 
 
